a9a7699cf968557c9121986c55b2f7dc63664198
jeroend <unknown>
2008-08-31 14:57:14 +0000

    Fix alias pull-down menu

--- userman_web/userman/forms/alias.py
+++ userman_web/userman/forms/alias.py
@@ -2,21 +2,26 @@ from django import newforms as forms
 from userman.model import user
 from userman.model import alias
 
+# The code under AddUserForm is only evaluated once at start-up, so if
+# we create the list of choices there it will never be changed even if
+# we add/remove items. The iterables below will be evaluated everytime
+# the form is displayed.
+class PossibleUsers:
+    def __iter__(self):
+        return iter([("","")] + [ (username, username) for username in user.GetAllUserNames() ])
+
+class PossibleAliases:
+    def __iter__(self):
+        return iter( [("","")] + [ (username, username) for username in alias.getAllAliasNames() ])
+
 class AliasForm(forms.Form):
     uid = forms.RegexField(regex="^[@.a-zA-Z\d \-\$]+$", required=False)
     cn = forms.RegexField(regex="^[^:^,]+$", required=False)
 
 class AddUserForm(forms.Form):
-    possibleUsers = [("","")]
-    possibleUsers += [ (username, username) for username in user.GetAllUserNames() ]
-    uid = forms.ChoiceField(choices=possibleUsers, required=False)
-    
-    possibleAliases = [("","")]
-    possibleAliases += [ (username, username) for username in alias.getAllAliasNames() ]
-    alias = forms.ChoiceField(choices=possibleAliases, required=False)
-
+    uid = forms.ChoiceField(choices=PossibleUsers(), required=False)
+    alias = forms.ChoiceField(choices=PossibleAliases(), required=False)
     email = forms.EmailField (required=False)
 
 class AddAliasForm(forms.Form):
     common_name = forms.RegexField(regex="^[a-zA-Z\d\-_\$]+$", required=True)
-        
