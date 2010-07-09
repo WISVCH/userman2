bd62559e476aa219e5fd945ca2b155e0b33bce4e
Jeroen Dekkers <jeroen@dekkers.ch>
2010-07-09 15:57:55 +0200

    Add hendrik

--- userman_web/userman/forms/user.py
+++ userman_web/userman/forms/user.py
@@ -13,7 +13,7 @@ class ChwarnRmForm(forms.Form):
     toBeDeleted = forms.DateTimeField(required=False)
 
 class ChprivForm(forms.Form):
-    server = forms.ChoiceField(choices=(("frans", "frans"), ("ank", "ank"), ("ch", "ch"), ("challenge", "challenge"), ("rob", "rob")))
+    server = forms.ChoiceField(choices=(("frans", "frans"), ("ank", "ank"), ("ch", "ch"), ("challenge", "challenge"), ("rob", "rob"), ("hendrik", "hendrik")))
     service = forms.ChoiceField(choices=(("sshd", "sshd"), ("samba", "samba"), ("cron", "cron")))
 
 class ChshForm(forms.Form):
