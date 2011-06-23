6f8367d351d7a1e97b1a4556df1d05213e9ee3ff
Mark Janssen <mark@praseodym.net>
2011-06-23 16:46:32 +0200

    Add computer account files as well

---
+++ userman_web/userman/forms/computer.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class AddComputerForm(forms.Form):
+    uid = forms.RegexField(regex="^[a-zA-Z][a-zA-Z\-_\d.]+$", error_message="UID entry must consist of alphanumeric characters, and be lower case")
---
+++ userman_web/userman/views/computers.py
@@ -0,0 +1,43 @@
+from userman.model import computer
+from django.shortcuts import render_to_response
+from django.http import Http404, HttpResponseRedirect
+from django.views.decorators.cache import cache_control
+from django.conf import settings
+
+from userman.forms.computer import *
+
+@cache_control(no_cache=True, must_revalidate=True)
+def displayComputers(request):
+    computers = computer.getAllComputers()
+    count = 0
+    for c in computers:
+        count += 1
+    
+    return render_to_response('computers.html', {'computers': computers, 'count': count})
+
+@cache_control(no_cache=True, must_revalidate=True)
+def addComputer(request):
+    if request.method == 'POST':
+        form = AddComputerForm(request.POST)
+        if form.is_valid():
+            uid = form.cleaned_data['uid'] + '$'
+            if computer.Exists(uid):
+                raise Http404
+            newComputer = computer.Add(uid)
+            
+            return HttpResponseRedirect(settings.USERMAN_PREFIX + '/computers/')
+    else:
+        form = AddComputerForm()
+    
+    return render_to_response('form.html', {'form': form, 'uid': "computers"})
+
+@cache_control(no_cache=True, must_revalidate=True)
+def rmComputer(request, cn):
+    try:
+        computerObj = computer.FromUID(cn)
+    except Exception, e:
+        raise Http404
+    
+    computerObj.connectRoot()
+    computerObj.remove()
+    return HttpResponseRedirect(settings.USERMAN_PREFIX + '/computers/')
