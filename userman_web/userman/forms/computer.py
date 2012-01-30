bc5e3fe1cf8aa2997060bf23fe4086610365877d
Mark Janssen <mark@praseodym.net>
2012-01-30 13:48:20 +0100

    Computer name must be lowercase

--- userman_web/userman/forms/computer.py
+++ userman_web/userman/forms/computer.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class AddComputerForm(forms.Form):
-    uid = forms.RegexField(regex="^[a-zA-Z][a-zA-Z\-_\d.]+$", error_message="UID entry must consist of alphanumeric characters")
+    uid = forms.RegexField(regex="^[a-z][a-z\-_\d.]+$", error_message="UID entry must consist of lowercase alphanumeric characters")
