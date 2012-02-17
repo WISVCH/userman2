a1af6905f0705c69f3dd5eb1e0058b486f1ae182
Mark Janssen <mark@praseodym.net>
2012-02-17 22:37:33 +0100

    Really fix email module this time

--- cron/mail.py
+++ cron/mail.py
@@ -5,11 +5,18 @@ from email.MIMEText import MIMEText
 
 def mailAdmin(subject, message):
     msg = MIMEText(message)
+    try:
+	from django.conf import settings
+        email = settings.ADMIN_MAIL
+    except ImportError:
+	import config
+        email = config.adminMail
+
     msg['Subject'] = '[Userman] ' + subject
-    msg['From'] = config.adminMail
-    msg['To'] = config.adminMail
+    msg['From'] = email
+    msg['To'] = email
 
     s = smtplib.SMTP()
     s.connect()
-    s.sendmail(config.adminMail, [config.adminMail], msg.as_string())
+    s.sendmail(email, [email], msg.as_string())
     s.close()
