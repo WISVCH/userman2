5e7647bddd5758d92f9487ddd60b144a558c7da6
Jeroen Dekkers <jeroen@dekkers.ch>
2012-09-25 23:58:24 +0200

    Make sure that the choices field of forms isn't cached

--- userman2/forms/alias.py
+++ userman2/forms/alias.py
@@ -14,13 +14,25 @@ class PossibleAliases:
     def __iter__(self):
         return iter( [("","")] + [ (username, username) for username in alias.getAllAliasNames() ])
 
+# The choices property of ChoiceField evaluates the choices field with a list(),
+# such that the iterator is only evaluated once. We don't want that, so we
+# override the property here.
+class NoCacheChoiceField(forms.ChoiceField):
+    def _get_choices(self):
+        return self._choices
+
+    def _set_choices(self, value):
+        self._choices = self.widget.choices = value
+
+    choices = property(_get_choices, _set_choices)
+
 class AliasForm(forms.Form):
     uid = forms.RegexField(regex="^[@.a-zA-Z\d\-_]+$", required=False)
     cn = forms.RegexField(regex="^[^:^,]+$", required=False)
 
 class AddUserForm(forms.Form):
-    uid = forms.ChoiceField(choices=PossibleUsers(), required=False)
-    alias = forms.ChoiceField(choices=PossibleAliases(), required=False)
+    uid = NoCacheChoiceField(choices=PossibleUsers(), required=False)
+    alias = NoCacheChoiceField(choices=PossibleAliases(), required=False)
     email = forms.EmailField (required=False)
 
 class AddAliasForm(forms.Form):
