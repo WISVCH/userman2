227fd347ab331636a6589843b4263f835898b1ff
Mark Janssen <mark@praseodym.net>
2017-01-07 23:18:22 +0100

    Update Dienst2 URL

--- userman2/views/users.py
+++ userman2/views/users.py
@@ -309,8 +309,8 @@ def dienst2(username, session):
         return {'status': 'whitelisted', 'message': 'Whitelisted'}
 
     headers = {'Authorization': 'Token ' + settings.DIENST2_APITOKEN}
-    url = 'https://frans.chnet/dienst2/ldb/api/v3/people/'
-    link_prefix = 'https://frans.chnet/dienst2/ldb/people/%d/'
+    url = 'https://dienst2.chnet/ldb/api/v3/people/'
+    link_prefix = 'https://dienst2.chnet/ldb/people/%d/'
     try:
         r = session.get(url, params={'ldap_username': username}, headers=headers)
     except requests.exceptions.RequestException as e:
