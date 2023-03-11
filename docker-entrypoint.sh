1d980845488fc67ab673c8708d547cf9adf3ec09
Mark Janssen <mark@praseodym.net>
2023-03-11 16:44:50 +0100

    Remove Datadog tracing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,7 @@
 set -e
 
 if [ "$1" = 'gunicorn' ]; then
-    export DATADOG_SERVICE_NAME=userman2
-    exec ddtrace-run "$@" -b 127.0.0.1:8000 userman2.wsgi
+    exec "$@" -b 127.0.0.1:8000 userman2.wsgi
 fi
 
 exec "$@"
